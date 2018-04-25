1d5c2ad2bfd6c7408a824855050670581af9d9eb
kazuya-seki <track613armory@gmail.com>
2018-04-25 22:46:18 +0900

    learned quantize scale

--- d3/src/app.js
+++ d3/src/app.js
@@ -11,3 +11,14 @@ const timeScale = d3
 
 console.log(timeScale(new Date())); // 100 を返す　ドメイン内の最大数だから
 console.log(timeScale.invert(50)); // 2016/1/1 と実行した時間の中間の時間を出力する
+
+// quantize scaleはドメインを rangeの数分に区切ってくれる
+// 例えば下記の例だと0 ~ 33.3333が red, 33.3333 ~ 66.666がwhiteみたいに自動で区切る
+// 多分パイチャートとか作る時に使うっぽい
+const quantizeScale = d3
+  .scaleQuantize() // quantize scaleはドメインを rangeの数分に区切ってくれる
+  .domain([0, 100])
+  .range(['red', 'white', 'green']); // 実際に出力する値の最小値〜最大値
+
+console.log(quantizeScale(22));
+console.log(quantizeScale(49));
