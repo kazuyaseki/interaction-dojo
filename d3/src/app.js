9af8056f4f8ff9d557d5c2e22bec76929f14fd10
kazuya-seki <track613armory@gmail.com>
2018-04-25 22:31:29 +0900

    studied linear scale

--- d3/src/app.js
+++ d3/src/app.js
@@ -1,1 +1,5 @@
-document.body.innerHTML = `<h1>Hello from ${d3.version}</h1>`;
+const linearScale = d3
+  .scaleLinear()
+  .domain([0, 100]) // 0% ~ 100%
+  .range([0, 1]) // 実際に出力する値の最小値〜最大値
+  .clamp(true); // linearScaleへの引数がrangeの最大値を超えちゃう場合に切り捨てする
